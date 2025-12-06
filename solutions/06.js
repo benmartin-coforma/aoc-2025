3f29ea0ee23a8ad9b9b34e1af1ca5fb593402089
benmartin-coforma <126210497+benmartin-coforma@users.noreply.github.com>
2025-12-06 08:15:10 -0700

    Refactor solution for day 6

--- solutions/06.js
+++ solutions/06.js
@@ -1,68 +1,64 @@
 (function () {
     const input = document.body.innerText;
 
-    function parse1 (input) {
+    /* Split the input by all-space columns.
+     * Example:
+     *   `qwe rty uio         [["qwe", "asd", "zx "],
+     *    asd fgh jkl    =>    ["rty", "fgh", "cv "],
+     *    zx  cv  bn `         ["uio", "jkl", "bn "]]
+     */
+    function * splitIntoProblems (input) {
         const lines = input.split("\n").filter(line => !!line);
-        const length = lines.map(line => line.length).reduce((m,x)=>m<x?m:x);
-        const groups = [...new Array(lines.length)].map(_ => []);
-        let lastSplitIndex = 0;
+        const length = lines[0].length;
+        let prevIndex = 0;
         for (let i = 0; i < length; i += 1) {
             if (lines.every(line => line[i] === " ")) {
-                for (let j = 0; j < lines.length; j += 1) {
-                    groups[j].push(lines[j].substring(lastSplitIndex, i));
-                }
-                lastSplitIndex = i + 1;
+                yield lines.map(line => line.substring(prevIndex, i));
+                prevIndex = i + 1;
             }
         }
-        for (let j = 0; j < lines.length; j += 1) {
-            groups[j].push(lines[j].substring(lastSplitIndex));
-        }
-        return groups;
-    }
-    
-    function partOne() {
-        const groups = parse1(input);
-        const numbers = groups.slice(0, -1)
-            .map(line => line.map(part => Number(part.trim())));
-        const operators = groups.at(-1).map(part => part.trim());
-        
-        return [...new Array(operators.length)].map((_, i) => {
-                const operands = numbers.map(ns => ns[i]);
-                const operator = operators[i];
-                const func = {
-                    "+": (sum, x) => sum + x,
-                    "*": (prod, x) => prod * x,
-                }[operator];
-                return operands.reduce(func);
-            })
-            .reduce((sum, x) => sum + x);
-    }
-    
-    function partTwo() {
-        const groups = parse1(input);
-        const numbers = groups.slice(0, -1);
-        const operators = groups.at(-1);
-
-        return [...new Array(operators.length)]
-            .map((_, i) => {
-                const operator = operators[i];
-                const operands = [];
-                for (let j = operator.length - 1; j >= 0; j -= 1) {
-                    const str = numbers.map(ns => ns[i][j]).join("").replaceAll(" ", "");
-                    if (str.length > 0)
-                        operands.push(Number(str));
-                }
-                const func = {
-                    "+": (sum, x) => sum + x,
-                    "*": (prod, x) => prod * x,
-                }[operator.trim()];
-                return operands.reduce(func);
-            })
-            .reduce((sum, x) => sum + x);
+        yield lines.map(line => line.substring(prevIndex));
     }
 
+    /* Read numbers left-to-right, top-to-bottom.
+     * Example:
+     * ["123","45 ","67 "] => [123, 45, 67]
+     */
+    function extractNumbers_partOne (numberStrings) {
+        return numberStrings.map(Number);
+    }
+
+    /* Read numbers top-to-bottom, left-to-right.
+     * Example:
+     * ["123","45 ","67 "] => [146, 257, 3]
+     */
+    function extractNumbers_partTwo (numberStrings) {
+        return [...new Array(numberStrings[0].length)]
+            .map((_, i) => Number(numberStrings.map(ns => ns[i]).join("")))
+    }
+
+    /* Determine the correct callback for `.reduce()`
+     */
+    function operation (operatorString) {
+        return { "+": SUM, "*": PRODUCT }[operatorString.trim()];
+    }
+    const PRODUCT = (product, x) => product * x;
+    const SUM = (sum, x) => sum + x;
+
+    /* How are these cephalapods ever going to learn
+     * if we keep doing their homework for them?
+     */
+    function homework (numberExtractor) {
+        return [...splitIntoProblems(input)]
+            .map(problemLines =>
+                numberExtractor(problemLines.slice(0, -1))
+                .reduce(operation(problemLines.at(-1)))
+            )
+            .reduce(SUM);
+    }
+    
     return {
-        partOne: partOne(),
-        partTwo: partTwo(),
+        partOne: homework(extractNumbers_partOne),
+        partTwo: homework(extractNumbers_partTwo),
     };
 })();
